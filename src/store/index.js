e8a6509c6adab9f5b2990d8b00e0a91810507479
Ersultan <example@gmail.com>
2023-02-07 21:03:54 +0300

    reduxToolkit createReducer

--- src/store/index.js
+++ src/store/index.js
@@ -1,29 +1,20 @@
-import { configureStore } from "@reduxjs/toolkit";
-import heroes from "../reducers/heroes";
-import filters from "../reducers/filters";
+import { configureStore } from '@reduxjs/toolkit';
+import heroes from '../reducers/heroes';
+import filters from '../reducers/filters';
 
 const stringMiddleware = () => (next) => (action) => {
-  if (typeof action === "string") {
-    return next({
-      type: action,
-    });
-  }
-  return next(action);
+    if (typeof action === 'string') {
+        return next({
+            type: action
+        })
+    }
+    return next(action)
 };
 
-// const store = createStore(
-//   combineReducers({ heroes: heroes, filters: filters }),
-//   compose(
-//     applyMiddleware(ReduxThunk, stringMiddleware),
-//     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-//   )
-// );
-
 const store = configureStore({
-  reducer: { heroes, filters },
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(stringMiddleware),
-  devTools: process.env.NODE_ENV !== "production",
-});
+    reducer: {heroes, filters},
+    middleware: getDefaultMiddleware => getDefaultMiddleware().concat(stringMiddleware),
+    devTools: process.env.NODE_ENV !== 'production',
+})
 
 export default store;
